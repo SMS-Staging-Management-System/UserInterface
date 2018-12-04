cd367051ac41735a019f56d9a05d67b24202023c
Paul Martella <paulmartella01@gmail.com>
2018-12-04 16:50:03 -0600

    test values removed good to merge

--- src/components/associatecontext/associatecheckin.component.tsx
+++ src/components/associatecontext/associatecheckin.component.tsx
@@ -8,6 +8,25 @@ export class AssociateCheckIn extends React.Component {
       testfield1: 'test',
       testfield2: 'current time'
     };
+    this.startTime = this.startTime.bind(this);
+    this.checkTime = this.checkTime.bind(this);
+  }
+
+  // method for getting system time I have no idea if this will work as intended
+  startTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = this.checkTime(m);
+    s = this.checkTime(s);
+    // document.getElementById('txt').innerHTML =
+    // h + ":" + m + ":" + s;
+    let t = setTimeout(this.startTime, 500);
+  }
+  checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
   }
 
   public render() {
--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -1,39 +1,10 @@
 import * as React from 'react';
 
 export class HomeComponent extends React.Component {
-  // testing values here
-  constructor(props) {
-    super(props);
-    this.state = {
-      testfield1: 'test',
-      testfield2: 'test',
-      testfield3: 'test',
-      testfield4: 'test'
-    };
-  }
-  // end test values
-
-
   public render() {
     return (
       <div>
         home
-        {/* Testing goes here only remove before final commit */}
-        {/* here users will input their whatevers for dah chicken */}
-        <div className="row" id="checkinheader">
-          <h4 className="col-sm">Check In</h4>
-          <h6 className="col-sm">Current Time Here</h6>
-        </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item flex-row-sb">
-              <input type="text" value="Description"></input>
-            </li>
-          </ul>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-danger" data-dismiss="modal">Check In</button>
-          </div>
-        {/* end chicken request */}
-        {/* End testing */}
       </div>
     );
   }
