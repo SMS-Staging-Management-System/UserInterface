0ec7314074796122e797a2296ed37ad06ce856d9
Paul Martella <paulmartella01@gmail.com>
2018-12-04 16:30:20 -0600

    started work on chechin component test stuff still in place DO NOT MERGE

--- src/components/associatecontext/associatecheckin.component.tsx
+++ src/components/associatecontext/associatecheckin.component.tsx
@@ -1,28 +1,21 @@
 import * as React from 'react';
 
-export class HomeComponent extends React.Component {
+export class AssociateCheckIn extends React.Component {
   // testing values here
   constructor(props) {
     super(props);
     this.state = {
       testfield1: 'test',
-      testfield2: 'test',
-      testfield3: 'test',
-      testfield4: 'test'
+      testfield2: 'current time'
     };
   }
-  // end test values
-
 
   public render() {
     return (
       <div>
-        home
-        {/* Testing goes here only remove before final commit */}
         {/* here users will input their whatevers for dah chicken */}
-        <div className="row" id="checkinheader">
-          <h4 className="col-sm">Check In</h4>
-          <h6 className="col-sm">Current Time Here</h6>
+        <div>
+          <h4>Check In</h4>
         </div>
           <ul className="list-group list-group-flush">
             <li className="list-group-item flex-row-sb">
@@ -33,9 +26,8 @@ export class HomeComponent extends React.Component {
             <button type="button" className="btn btn-danger" data-dismiss="modal">Check In</button>
           </div>
         {/* end chicken request */}
-        {/* End testing */}
       </div>
+      
     );
   }
 }
-
--- src/components/associatecontext/associatecontext.component.tsx
+++ src/components/associatecontext/associatecontext.component.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 
-export class HomeComponent extends React.Component {
+export class AssociateContext extends React.Component {
+  // testing values here
+  constructor(props) {
+    super(props);
+    this.state = {
+      testfield1: 'test',
+      testfield2: 'test',
+      testfield3: 'test',
+      testfield4: 'test'
+    };
+  }
+  // end test values
 
   public render() {
     return (
       <div>
-        {/* Check-In or chicken depending on how hungry you are */}
+        {/* Chech-In, Chechneya, or chicken depending on how hungry you are */}
         <div id="rowboxthing">
           <div id="accordion">
 
@@ -31,7 +42,7 @@ export class HomeComponent extends React.Component {
                 </h5>
               </div>
 
-              <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+              <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                 <div className="card-body">
                   Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                 </div>
