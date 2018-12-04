1dc715c9dcdebd2d5cb7caad479448c6c7b3ee78
Paul Martella <paulmartella01@gmail.com>
2018-12-04 13:15:17 -0600

    try it now

---
+++ src/components/associatecontext/associatecontext.component.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export class HomeComponent extends React.Component {
+
+  public render() {
+    return (
+      <div>
+        {/* Testing goes here only remove before final commit */}
+        {/* Check-In or chicken depending on how hungry you are */}
+        <div id="row"> 
+          {/* Table goes somewhere here at some point eventually? */}
+            <div>first</div><div>last</div><div>date</div><div>cohort</div>
+        </div>
+        {/* End Chechneya */}
+      </div>
+      
+    );
+  }
+}
