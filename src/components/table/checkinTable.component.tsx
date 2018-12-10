089f622b3753847afc00ea1a8adc035dd76f79b2
Calvin Vo <cvogit@gmail.com>
2018-12-10 09:21:06 -0600

    Shared tale for checkins between manager aand associate

--- src/components/table/checkinTable.component.tsx
+++ src/components/table/checkinTable.component.tsx
@@ -3,18 +3,32 @@ import CheckInHeaderComponent from '../manager/checkin/check-in-header.component
 import { Table } from 'reactstrap';
 import Paginate from '../manager/pagination.component';
 
+interface IProps {
+  type: string;
+}
 /**
  * The table to render a list of check in
  */
-export class CheckinTableComponent extends React.Component {
+export class CheckinTableComponent extends React.Component<IProps, {}> {
+
+  public renderRows = () => {
+    if(this.props.type === 'manager') {
+      return <Paginate/>
+    } else if(this.props.type === 'associate') {
+      // Return your associate rows here
+      return <></>
+    }
+    return <></>
+  }
 
   public render() {
+    const sRows = this.renderRows();
     return (
       <>
         <Table className="table table-hover table-bordered">
           <CheckInHeaderComponent/> 
           <tbody>
-            <Paginate/>
+            {sRows}
           </tbody>
         </Table>
       </>
