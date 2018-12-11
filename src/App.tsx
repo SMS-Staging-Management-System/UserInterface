19687c261eda2dcbfc0d34f52540a62d36c345b1
Calvin Vo <cvogit@gmail.com>
2018-12-11 15:58:02 -0600

    Revert "Assocaite content"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import './include/bootstrap';
 import './App.css';
-import './Associate.css';
 import AppNav from './components/nav/nav.component';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { AssociateContent } from './components/associate/associatecontent.component';
 import { Provider } from 'react-redux';
 import { store } from './Store';
 import { ToastContainer } from "react-toastify";
