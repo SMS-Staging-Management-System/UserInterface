26b920fa3a2626e0ffbf8f1cd25f2541780e0f36
Blake Kruppa <btkruppa513@gmail.com>
2018-12-17 10:43:23 -0600

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -6,10 +6,10 @@ const dev = {
 };
 
 const prod = {
-  awsRegion:          'us-west-2',
+  awsRegion:          'us-east-1',
   cognitoClientId:    'n09bpbndlp78jrbv6rbar4d13',
   cognitoUserPoolId:  'us-east-1_xavxFp1nr',
-  smsContext:         'http://znc.anorexicseal.com:8765'
+  smsContext:         'https://7i6rudc3m2.execute-api.us-east-1.amazonaws.com/prod'
 };
 
 export const environment = process.env.NODE_ENV === "production" ? prod : dev;
