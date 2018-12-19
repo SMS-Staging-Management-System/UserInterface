425654d639cc3f9561d06c7a67e0f59128a17be0
kurisu <callmekurisu@gmail.com>
2018-12-19 14:33:13 -0600

    added checkin by id table on manager cohort tab

--- src/components/manager/cohort/associates-row.component.tsx
+++ src/components/manager/cohort/associates-row.component.tsx
@@ -1,32 +1,53 @@
 import * as React from "react";
 import { IUser } from "src/model/User.model";
-
+import { AssociateRowCheckinComponent } from "./associate-row-checkins.component";
 /**
  * The class row
  */
 
 interface IProps {
-  user: IUser
+  user: IUser,
 }
 
-export class AssociatesRowComponent extends React.Component<IProps> {
-
+interface IComponentState {
+  modal: boolean
+}
+export class AssociatesRowComponent extends React.Component<IProps,IComponentState> {
+  constructor(props){
+    super(props);
+    this.state = {
+      modal: false
+    }
+  }
   public handleClick = () => {
-    // const cohort = document.getElementById("cohort-associates") as HTMLElement;
+    this.setState({
+      ...this.state,
+      modal: true
+    })
+  }
 
-    // cohort.style.width = "400px";
+  public modalOff = () => {
+    this.setState({
+      ...this.state,
+      modal: false
+    })
   }
 
   public render() {
     return (
       <>
-          <tr className="" id={`row-${this.props.user.userId}`} onClick={() => this.handleClick()}>
+          <tr className="" id={`row-${this.props.user.userId}`} 
+            onClick={() => this.handleClick()}>
             <td>{this.props.user.userId}</td>
             <td>{this.props.user.firstName}</td>
             <td>{this.props.user.lastName}</td>
             <td>{this.props.user.email}</td>
           </tr>
-        
+         {/* Modal for manager comments */}
+         <AssociateRowCheckinComponent
+           modal={this.state.modal}
+           modalOff={this.modalOff}
+           userId={this.props.user.userId}/>
       </>
     );
   }
