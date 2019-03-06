55ea16cdc522a4876c688f4161c18332cb6322ed
Aaron Gravelle <agrav12825@gmail.com>
2019-03-06 16:39:28 -0500

    switch placeholders to values

--- src/components/profile/profile.component.tsx
+++ src/components/profile/profile.component.tsx
@@ -15,43 +15,43 @@ class Profile extends Component<IProfileProps, any> {
             <Col md={4}>
               <FormGroup>
                 <Label for="currentSMSUserEmail">Email</Label>
-                <Input type="email" name="email" id="currentSMSUserEmail" placeholder={this.props.user.email} />
+                <Input type="email" name="email" id="currentSMSUserEmail" value={this.props.user.email} />
               </FormGroup>
              </Col>
              <Col md={4}>
               <FormGroup>
                 <Label for="currentSMSUserFirstname">Firstname</Label>
-                <Input type="text" name="firstName" id="currentSMSUserFirstname" placeholder={this.props.user.firstName} />
+                <Input type="text" name="firstName" id="currentSMSUserFirstname" defaultValue={this.props.user.firstName} />
               </FormGroup>
             </Col>
             <Col md={4}>
               <FormGroup>
                 <Label for="currentSMSUserLastname">Lastname</Label>
-                <Input type="text" name="lastName" id="currentSMSUserLastname" placeholder={this.props.user.lastName} />
+                <Input type="text" name="lastName" id="currentSMSUserLastname" defaultValue={this.props.user.lastName} />
               </FormGroup>
             </Col>
           </Row>
         <FormGroup>
           <Label for="currentSMSUserAddress">Address</Label>
-          <Input type="text" name="address" id="currentSMSUserAddress" placeholder={this.props.user.address.street}/>
+          <Input type="text" name="address" id="currentSMSUserAddress" defaultValue={this.props.user.address.street}/>
         </FormGroup>
         <Row>
           <Col md={6}>
             <FormGroup>
               <Label for="currentSMSUserCity">City</Label>
-              <Input type="text" name="city" id="currentSMSUserCity" placeholder={this.props.user.address.city}/>
+              <Input type="text" name="city" id="currentSMSUserCity" defaultValue={this.props.user.address.city}/>
             </FormGroup>
           </Col>
           <Col md={4}>
             <FormGroup>
               <Label for="currentSMSUserState">State</Label>
-              <Input type="text" name="state" id="currentSMSUserState" placeholder={this.props.user.address.state}/>
+              <Input type="text" name="state" id="currentSMSUserState" defaultValue={this.props.user.address.state}/>
             </FormGroup>
           </Col>
           <Col md={2}>
             <FormGroup>
               <Label for="currentSMSUserZip">Zip</Label>
-              <Input type="text" name="zip" id="currentSMSUserState" placeholder={this.props.user.address.zip}/>
+              <Input type="text" name="zip" id="currentSMSUserState" defaultValue={this.props.user.address.zip}/>
             </FormGroup>  
           </Col>
         </Row>
