811b476e7f9d1e9d03e6d821eea01fd2bcf52f61
Michael R Roark <michaelrroark@gmail.com>
2019-03-06 11:30:43 -0500

    Framed the InterviewList component. Need to hook to react, and api.

---
+++ src/interview-src/component/InterviewList/InterviewList.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Jumbotron from 'reactstrap/lib/Jumbotron';
+import Table from 'reactstrap/lib/Table';
+import { connect } from 'react-redux';
+import { IState } from '../../../reducers';
+
+export interface InterviewListProps {
+    listOfInterviews : any[]
+}
+ 
+export interface InterviewListState {
+    
+}
+ 
+class InterviewList extends React.Component<InterviewListProps, InterviewListState> {
+    constructor(props: InterviewListProps) {
+        super(props);
+    }
+
+    renderListOfInterviews = () => {
+        if(this.props.listOfInterviews[0]){
+            const result = this.props.listOfInterviews.map(() => {
+                <tr>
+                    <td></td>
+                </tr>
+            })
+
+            return result;
+        } else {
+            return <p></p>
+        }
+    }
+
+    render() { 
+        return ( 
+            <Jumbotron>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                        </tr>
+                    </thead>
+                {this.renderListOfInterviews()}
+                </Table>
+            </Jumbotron>
+         );
+    }
+}
+
+const mapStateToProps = (state: IState) => {
+    return {
+
+    }
+}
+ 
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(InterviewList);
