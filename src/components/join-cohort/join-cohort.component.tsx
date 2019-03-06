38db494dbcbfa0eef861c891181a60bbd3ed9146
Abatson <abatson94@gmail.com>
2019-03-06 14:26:45 -0500

    skeleton

--- src/components/join-cohort/join-cohort.component.tsx
+++ src/components/join-cohort/join-cohort.component.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 
 
 interface IJoinCohortProps {
+    urlparam: string,
+
 
 }
 
@@ -11,7 +13,28 @@ export class JoinCohortComponent extends React.Component<IJoinCohortProps, any> 
         super(props)
     }
 
+    //assert cohort token is real
+    //if not display not a valid cohort link,
 
+    //If already logged in take to join cohort window
+
+    //Offer login or signup for the current cohort
+    //on successful login/signup, take to join cohort window
+
+    
+    //join cohort window has username and cohort name and a join button
+    //after clicking join, take you to cohort page
+
+    render(){
+
+
+
+        return(
+            <div>
+
+            </div>
+        )
+    }
 
 
 
