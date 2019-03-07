2a0f85c754d5c0f32abc591ca77a3f631cf26b43
Juan <juan.melendez3.14@gmail.com>
2019-03-07 16:04:12 -0500

    added prototype for cohorts table

--- src/components/manage/manage-cohorts/manage-cohorts.component.tsx
+++ src/components/manage/manage-cohorts/manage-cohorts.component.tsx
@@ -6,9 +6,37 @@ import {
 import { ICognitoUser } from '../../../model/cognito-user.model';
 import Button from 'reactstrap/lib/Button';
 
+/**
+ * {v}: dropdown with further info
+ * #: hoverable props
+ * 
+ * Row headers:
+ * |-------------------------------------------------------------------------------------------------|
+ * |--Cohort.Name--|--Address.alias{v}--|--Token.(){v}-- |--StartMonth--|--trainer email-v--         |
+ * |=================================================================================================|
+ * |  1901-blake   |  USF               | [Get token  v] | March 2019   | [blake.kruppa@gmail.com v] |
+ * |-------------------------------------------------------------------------------------------------|
+ * |  1902-flake   |  Reston            | [Get token  v] | March 2019   | [flake@gmail.com v       ] |
+ * |-------------------------------------------------------------------------------------------------|
+ * |  1903-fake    |  USF               | [Get token  v] | March 2019   | [abatson@gmail.com v     ] |
+ * |-------------------------------------------------------------------------------------------------|
+ * |  1904-bake    |  Reston            | [Get token  v] | March 2019   | [fllorida.man@gmail.com v] |
+ * |-------------------------------------------------------------------------------------------------|
+ * |  1905-make    |  USF               | [Get token  v] | March 2019   | [blake.kruppa@gmail.com v] |
+ * |-------------------------------------------------------------------------------------------------|
+ *                                                                         [p1 ] [p2 ] ... [p4 ] [p5 ]                
+ * 
+ * {
+ *   Cohort # {
+ *     cohortDescription,
+ *   }
+ * 
+ * }
+ */
 export interface IManageAssociatesComponentProps {
   manageUsers: ICognitoUser[];
-  toggleCreateCohortModal: () => void
+
+  toggleCreateCohortModal: () => void;
 }
 
 export class ManageCohortsComponenet extends React.Component<IManageAssociatesComponentProps, any> {
