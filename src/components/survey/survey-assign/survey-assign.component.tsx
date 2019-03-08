dec427788bb62aa373f9e48dd03a8723e68e3b35
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-03-08 10:16:28 -0500

    Added style for Assign nav item

--- src/components/survey/survey-assign/survey-assign.component.tsx
+++ src/components/survey/survey-assign/survey-assign.component.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Table, Input } from 'reactstrap';
 
 export class SurveyAssignComponent extends React.Component {
     constructor(props) {
@@ -6,13 +7,47 @@ export class SurveyAssignComponent extends React.Component {
     }
 
     render() {
-        return (
 
-            <div className="container create-survey-container" >
-                <div className="jumbotron">
-                    <h2>Survey Assign Component</h2>
+        return (
+            <Fragment>
+                <Table striped id="manage-users-table" className="tableUsers">
+                    <thead className="rev-background-color" >
+                        <tr >
+                            <th>Select</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="rev-table-row">
+                            <td><Input type="checkbox" /></td>
+                            <td>Dunieski</td>
+                            <td>Otano</td>
+                            <td>dunieskior@gmail.com</td>
+                        </tr>
+                        <tr className="rev-table-row">
+                            <td><Input type="checkbox" /></td>
+                            <td>Dunieski</td>
+                            <td>Otano</td>
+                            <td>dunieskior@gmail.com</td>
+                        </tr>
+                        {/* {
+              this.props.manageUsers.map((user) =>
+                <tr key={user.email} className="rev-table-row">
+                  <td></td>
+                  <td></td>
+                  <td>{user.email}</td>
+                </tr>
+              )
+            } */}
+
+                    </tbody>
+                </Table>
+                <div className="buttonDiv">
+                    <tr><button type="submit" className="assignSurveyBtn">Assign Survey</button></tr>
                 </div>
-            </div>
+            </Fragment>
         );
     }
 }
