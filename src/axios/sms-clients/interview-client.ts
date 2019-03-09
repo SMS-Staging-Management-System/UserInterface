290ffa03df738fc3220dc63eebff992c2d7a89b2
Michael R Roark <michaelrroark@gmail.com>
2019-03-09 15:54:24 -0500

    Finished the InterviewList component.

--- src/axios/sms-clients/interview-client.ts
+++ src/axios/sms-clients/interview-client.ts
@@ -6,5 +6,16 @@ const interviewContext = '/interview';
 export const interviewClient = {
     testfetch: async () => {
         return await smsClient.get(interviewContext);
+    },
+    fetchPage: (pageNumber? : number, pageSize? : number, orderBy = 'id', direction='ASC') => {
+        let url = interviewContext;
+        url += '/page?orderBy=' + orderBy + '&direction=' + direction;
+        if (pageNumber) {
+            url+='&pageNumber=' + pageNumber;
+        }
+        if (pageSize) {
+            url += '&pageSize=' + pageSize;
+        }
+        return smsClient.get(url);
     }
 }
