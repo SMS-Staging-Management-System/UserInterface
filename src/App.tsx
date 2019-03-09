cb2e4607601ecb2ab8ff5ebad810169e345f8982
kennethcurrie <kjcurrie@comcast.net>
2019-03-09 15:48:29 -0500

    revert app.tsx to base

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
 import { store } from './Store';
 import AppNav from './components/nav/nav.component';
-import { InterviewPerAssoc } from './interview-src/component/interviewsPerAssocComponent/interviewsPerAssocComponent.component';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter,  Switch, Route } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 
 import { ToastContainer, toast } from 'react-toastify';
+import { ManagementRoutes } from './routes/Management.routes';
+import { SurveyRoutes } from './routes/Survey.routes';
+import { InterviewRoutes } from './routes/Interview.routes';
 
 class App extends React.Component {
   public render() {
@@ -19,7 +21,11 @@ class App extends React.Component {
           <div>
             <AppNav />
             <div id="main-content-container">
-              <InterviewPerAssoc />
+              <Switch>
+                  <Route path='/management' component={ManagementRoutes}/>
+                  <Route path='/interview' component={InterviewRoutes}/>
+                  <Route path='/surveys' component ={SurveyRoutes}/>
+              </Switch>
             </div>
             <ToastContainer autoClose={2000} position={toast.POSITION.BOTTOM_RIGHT}/>
           </div>
