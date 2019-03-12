0407e0d45f9c8478a5604c6e1013a3a66042fba0
csfrancis94 <csfrancis@email.com>
2019-03-12 10:24:11 -0400

    App.tsx revert

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,27 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
 import { store } from './Store';
 import AppNav from './components/nav/nav.component';
-import { BrowserRouter,  Switch, } from 'react-router-dom';//Route } from 'react-router-dom';
+import { BrowserRouter,  Switch, Route } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 
 import { ToastContainer, toast } from 'react-toastify';
-// import { ManagementRoutes } from './routes/Management.routes';
-// import { SurveyRoutes } from './routes/Survey.routes';
-// import { InterviewRoutes } from './routes/Interview.routes';
-import { ReportForm } from './interview-src/component/report-form/ReportForm.container'
+import { ManagementRoutes } from './routes/Management.routes';
+import { SurveyRoutes } from './routes/Survey.routes';
+import { InterviewRoutes } from './routes/Interview.routes';
 
 class App extends React.Component {
   public render() {
     return (
-      
       <Provider store={store}>
         <BrowserRouter>
           <div>
             <AppNav />
             <div id="main-content-container">
               <Switch>
-                <ReportForm></ReportForm>
-                  {/* <Route path='/management' component={ManagementRoutes}/>
+                  <Route path='/management' component={ManagementRoutes}/>
                   <Route path='/interview' component={InterviewRoutes}/>
-                  <Route path='/surveys' component ={SurveyRoutes}/> */}
+                  <Route path='/surveys' component ={SurveyRoutes}/>
               </Switch>
             </div>
             <ToastContainer autoClose={2000} position={toast.POSITION.BOTTOM_RIGHT}/>
