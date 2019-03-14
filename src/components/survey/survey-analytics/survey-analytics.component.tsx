9022892bc107c4fcedb0eabb72b7bf91eeb4a936
Kyle Serrecchia <kyserrecchia@gmail.com>
2019-03-14 10:30:27 -0400

    analytics added to my-surveys

--- src/components/survey/survey-analytics/survey-analytics.component.tsx
+++ src/components/survey/survey-analytics/survey-analytics.component.tsx
@@ -1,62 +1,64 @@
-import React from 'react';
-import { Table } from 'reactstrap';
-import { surveyClient } from '../../../axios/sms-clients/survey-client';
-import { Redirect } from 'react-router';
+// NO LONGER IN USE
 
-export class SurveyAnalyticsComponent extends React.Component<any, any> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            surveys: [],
-            surveysLoaded: false,
-            redirectTo: null
-        }
-    }
+// import React from 'react';
+// import { Table } from 'reactstrap';
+// import { surveyClient } from '../../../axios/sms-clients/survey-client';
+// import { Redirect } from 'react-router';
 
-    componentDidMount() {
-        this.loadAllSurveys();
-    }
+// export class SurveyAnalyticsComponent extends React.Component<any, any> {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             surveys: [],
+//             surveysLoaded: false,
+//             redirectTo: null
+//         }
+//     }
 
-    loadAllSurveys = async () => {
-        const surveys = await surveyClient.findAllSurveys();
-        if (surveys) {
-            this.setState({
-                surveys: surveys,
-                surveysLoaded: true
-            });
-        }
-    }
+//     componentDidMount() {
+//         this.loadAllSurveys();
+//     }
 
-    handleLoadSurveyData = (surveyId: number) => {
-        this.setState({
-            redirectTo: `/surveys/survey-data/${surveyId}`
-        })
-    }
+//     loadAllSurveys = async () => {
+//         const surveys = await surveyClient.findAllSurveys();
+//         if (surveys) {
+//             this.setState({
+//                 surveys: surveys,
+//                 surveysLoaded: true
+//             });
+//         }
+//     }
 
-    render() {
-        if (this.state.redirectTo) {
-            return <Redirect push to={this.state.redirectTo} />
-        }
-        return (
-            <Table striped id="manage-users-table" className="tableUsers">
-                <thead className="rev-background-color">
-                <tr>
-                    <th>Title</th>
-                    <th>Description</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {this.state.surveys.map(survey => (
-                            <tr key={survey.surveyId} className="rev-table-row" onClick={() => this.handleLoadSurveyData(survey.surveyId)}>
-                                <td>{survey.title}</td>
-                                <td>{survey.description}</td>
-                            </tr>
-                    ))}
-                </tbody>
-            </Table>
-        );
-    }
-}
+//     handleLoadSurveyData = (surveyId: number) => {
+//         this.setState({
+//             redirectTo: `/surveys/survey-data/${surveyId}`
+//         })
+//     }
+
+//     render() {
+//         if (this.state.redirectTo) {
+//             return <Redirect push to={this.state.redirectTo} />
+//         }
+//         return (
+//             <Table striped id="manage-users-table" className="tableUsers">
+//                 <thead className="rev-background-color">
+//                 <tr>
+//                     <th>Title</th>
+//                     <th>Description</th>
+//                 </tr>
+//                 </thead>
+//                 <tbody>
+//                     {this.state.surveys.map(survey => (
+//                             <tr key={survey.surveyId} className="rev-table-row" onClick={() => this.handleLoadSurveyData(survey.surveyId)}>
+//                                 <td>{survey.title}</td>
+//                                 <td>{survey.description}</td>
+//                             </tr>
+//                     ))}
+//                 </tbody>
+//             </Table>
+//         );
+//     }
+// }
 
 
-export default SurveyAnalyticsComponent;
+// export default SurveyAnalyticsComponent;
