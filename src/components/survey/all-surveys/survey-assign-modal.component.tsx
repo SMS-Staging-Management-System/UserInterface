a0b6e4392bfaf346e8ed2e0d0cae8150005e4a8e
Kyle Serrecchia <kyserrecchia@gmail.com>
2019-03-18 16:28:21 -0400

    cleaned up console.logs and added comments

--- src/components/survey/all-surveys/survey-assign-modal.component.tsx
+++ src/components/survey/all-surveys/survey-assign-modal.component.tsx
@@ -5,7 +5,6 @@ import { cohortClient } from '../../../axios/sms-clients/cohort-client';
 import { userClient } from '../../../axios/sms-clients/user-client';
 import { surveyClient } from '../../../axios/sms-clients/survey-client';
 // import Loader from '../Loader/Loader';
-// import createCohortModalContainer from '../../manage/create-cohort-modal/create-cohort-modal.container';
 
 
 interface IComponentProps {
@@ -41,15 +40,39 @@ class SurveyModal extends React.Component<IComponentProps, IComponentState> {
   }
 
     componentDidMount() {
+        // grabs all cohorts from database and loads them into
+        //   the userArray, each having their cohort id and email address
         this.loadAllCohorts();
+    }
+
+
+
+    loadAllCohorts = async () => {
+        // get all cohorts
+        const cohorts = await cohortClient.findAll();
+        if (cohorts) {
+            this.setState({
+                cohorts: cohorts.data,
+                cohortsLoaded: true
+            }, 
+            // then, only AFTER state is changed, load user emails according from cohort data
+                ()=>{
+                    this.loadAllUserEmails();
+                });
+        }
     }
 
     loadAllUserEmails = async () => {
         const { cohorts } = this.state;
+        // set up array to dump into state 
         const idAndEmailArray: IUserCohortIdAndEmail[] = [];
+
+        // for each cohort, get users
         for (const cohort of cohorts) {
             const users = await userClient.findAllByCohortId(cohort.cohortId);
+            // for each array of users, load into array
             for (const user of users.data) {
+                // make object for each user to push to array
                 const idAndEmailObj: IUserCohortIdAndEmail = {
                     id: cohort.cohortId,
                     email: user.email
@@ -60,59 +83,49 @@ class SurveyModal extends React.Component<IComponentProps, IComponentState> {
 
         this.setState({
             userArray: idAndEmailArray
-        }, 
-            () => {
-                console.log(this.state.userArray);
-            });
-    }
-
-    loadAllCohorts = async () => {
-        const cohorts = await cohortClient.findAll();
-        await console.log('cohorts:');
-        await console.log(cohorts.data);
-        if (cohorts) {
-            this.setState({
-                cohorts: cohorts.data,
-                cohortsLoaded: true
-            }, 
-                ()=>{
-                    this.loadAllUserEmails();
-                });
-        }
+        });
     }
 
     checkFunc = (e) => {
+        // this function is for the the Select ALL checkbox
         const { checked } = e.target;
+        const { emailsToAssign: emAssign } = this.state;
         const id = +e.target.id;
         let emailArray:string[] = [];
-        const { emailsToAssign: emAssign } = this.state;
-        if (checked) {
 
+        if (checked) {
+            // filter out users for only ones in THIS cohort
             this.state.userArray.filter(user => {
                 return user.id === id
             }).map(user => {
+                // for each user in this cohort, dump them into array to assign to
+                // but first, check they are not already in the array
                 if (!this.state.emailsToAssign.includes(user.email)) {
                     emailArray.push(user.email);
                 }
+                // then check all individual user checkboxes in this cohort 
                 let el = document.getElementById(user.email) as HTMLInputElement;
                 if (el) { el.checked = true; }
             }); 
-
             this.setState({
                 emailsToAssign: this.state.emailsToAssign.concat(emailArray)
             });
 
         }  else {
-
+            // if checking OFF the Select ALL, do this
+            // filter through all users for those in this checkboxes cohort
             this.state.userArray.filter(user => {
                 return user.id === id
             }).map(user => {
+                // push into local array to know which users to remove from state below
                 emailArray.push(user.email);
+                // uncheck each individual user checkbox if all is unchecked
                 let el = document.getElementById(user.email) as HTMLInputElement;
                 if (el) { el.checked = false; }
             });
 
             this.setState({
+                // filter emailsToAssign:emAssign array to get rid of all emails from local emailArray
                 emailsToAssign: emAssign.filter(em => {
                     let inArr:boolean = true;
                     for (const email of emailArray) {
@@ -128,14 +141,20 @@ class SurveyModal extends React.Component<IComponentProps, IComponentState> {
     }
 
     checkUserFunc = (e) => {
+        // this function is for each individual user email checkbox 
         const { id: email, checked } = e.target;
+
         if (checked) {
+            // first check they are not already in the emailsToAssign array,
+            //    if not, add them to it in state
             if (!this.state.emailsToAssign.includes(email)) {
                 this.setState({
                     emailsToAssign: [...this.state.emailsToAssign, email]
                 });
             }
         } else {
+            // first check they are already in the emailsToAssign array,
+            //   if yes, then remove them from it by filtering them out
             if (this.state.emailsToAssign.includes(email)) {
                 this.setState({
                     emailsToAssign: this.state.emailsToAssign.filter(emailToAssign => {
@@ -143,30 +162,24 @@ class SurveyModal extends React.Component<IComponentProps, IComponentState> {
                     })
                 });
 
-                
+                // If they have been checked off, then Select ALL for their cohort should now be off
+                //  -this achieves that by looping through the array and finding their associated
+                //      cohort by id and then checking false on its associated input element
                 let cohortId = '';
                 for (const user of this.state.userArray) {
                     if (user.email === email) {
                         cohortId = user.id.toString();
                     }
                 }
-                console.log(`cohortId`);
-                console.log(cohortId)
                 let el = document.getElementById(cohortId) as HTMLInputElement;
                 if (el) { el.checked = false; }
             }
         }
     }
 
-    componentDidUpdate() {
-        console.log(this.state.emailsToAssign);
-    }
-
     postSurveyToCohort = () => {
-        console.log(`survey ids: ${this.props.surveysToAssign}`);
-        console.log(`cohort ids: ${this.state.cohortIdsToAssign}`);
-        // this.loadCohortUsersToAssign();
-
+        // loop through each chosen survey from parent component and for each email,
+        //   assign the survey to the history table
         for ( const surveyId of this.props.surveysToAssign) {
             for ( const email of this.state.emailsToAssign) {
             surveyClient.assignSurveyByIdAndEmail(surveyId, email);
@@ -174,16 +187,13 @@ class SurveyModal extends React.Component<IComponentProps, IComponentState> {
         }
     }
 
+    // toggles the modal and clears the emailsToAssign
   toggle = () => {
     this.setState(prevState => ({
       modal: !prevState.modal,
       emailsToAssign: []
     }));
   }
-
-//   toggleDropdown = (i) => {
-
-//   }
 
   render() {
     return (
