5df3b4d772dfe5d1ea42bbf1297440330b4fff3f
JPGomesGoncalves <JPGomesGoncalves@student.fullsail.edu>
2019-03-19 11:20:14 -0400

    I reduxed the survey, you'r welcome

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,17 +1,19 @@
 import { combineReducers } from 'redux';
 import { IManagementState, managementState } from './management';
 import { IInterviewState, interviewState } from './interview';
+import { ISurveyState, surveyState } from './survey';
 // import { ISurveyState, surveyState } from './survey';
 
 
 export interface IState {
     managementState: IManagementState,
     interviewState: IInterviewState,
+    surveyState : ISurveyState
     // surveyState: ISurveyState
 }
 
 export const state = combineReducers<IState>({
     managementState,
     interviewState,
-    // surveyState
+    surveyState
 })
---
+++ src/reducers/survey/SurveyBuild.reducer.ts
@@ -0,0 +1,52 @@
+import { ISurveyBuildState } from ".";
+import { surveyBuildTypes } from "../../actions/survey/SurveyBuild.action";
+
+const initialState: ISurveyBuildState = {
+    dummySurvey: {
+        surveyId: 1,
+        title: 'not filled',
+        description: 'Example Survey 1 Description',
+        dateCreated: new Date(),
+        closingDate: null,
+        template: false,
+        published: true
+    },
+    dummyQuestionArray: [],
+    dummyAnswerArray: [],
+    parsedAnswers: [],
+    junctionTable: {
+        id: 0,
+
+        questionId: {
+            questionId: 0,
+            question: 'not filled',
+            typeId: 0,
+        },
+        questionOrder: 0,
+
+        surveyId: {
+            surveyId: 0,
+            title: 'not fileld',
+            description: 'not filled',
+            dateCreated: new Date(),
+            closingDate: null,
+            template: false,
+            published: false,
+        }
+    }
+}
+
+export const surveyBuildReducer = (state = initialState, action: any): ISurveyBuildState => {
+    switch (action.type) {
+        case surveyBuildTypes.CreatSurvey:
+            return {
+                ...state,
+                dummySurvey: action.payload.actionSurvey,
+                dummyQuestionArray: action.payload.actionQuestion,
+                dummyAnswerArray: action.payload.actionAnswer,
+                parsedAnswers: action.payload.actionParsedAnswers,
+                junctionTable: action.payload.actionJunctionTable,
+            }
+    }
+    return state;
+}
