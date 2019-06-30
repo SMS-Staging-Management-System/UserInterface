179b647b5897f3661e1c274f6b185e94226bc02b
micheledexter <micheleannedexter@gmail.com>
2019-06-30 16:04:26 -0400

    allow access to client information from interview since it's there already

--- src/model/Interview.model.ts
+++ src/model/Interview.model.ts
@@ -1,16 +1,18 @@
 import { InterviewFeedback } from "./Interview.feedback";
 import { IAssociateInput } from "./Associateinput.model";
+import { IClient } from "./Client.model";
 
 export interface Interview {
-  interviewId:      number,
-  managerId: number,
-  associateId: number,
-  scheduled: Date,
-  notified: Date,
-  reviewed: Date,
-  place: string,
-  feedback : InterviewFeedback,
-  associateInput : IAssociateInput
+  interviewId: number;
+  managerId: number;
+  associateId: number;
+  scheduled: Date;
+  notified: Date;
+  reviewed: Date;
+  place: string;
+  feedback : InterviewFeedback;
+  associateInput : IAssociateInput;
+  client: IClient;
 }
 
 /*
