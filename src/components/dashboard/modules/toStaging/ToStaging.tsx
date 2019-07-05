5e9411917e3a5b0dbe5d808e650054f2aa8cd76b
Michele Dexter <micheleannedexter@gmail.com>
2019-07-05 10:49:22 -0400

    get cohort information on component mount

--- src/components/dashboard/modules/toStaging/ToStaging.tsx
+++ src/components/dashboard/modules/toStaging/ToStaging.tsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 import { RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { IState } from '../../../../reducers';
+import { ICohort } from '../../../../model/cohort';
+import { getCohorts } from '../../../../actions/dashboardActions/to-staging.actions'
 
 interface IToStagingProps extends RouteComponentProps<{}> {
     WrappedComponent: any;
+    cohortList: ICohort[];
+    getCohorts: (date: Date | number) => void;
 }
 class ToStaging extends Component<IToStagingProps,any> {
+
+    onComponentDidUpdate = () => {
+        this.props.getCohorts(new Date());
+    }
+    
     render() {
         return (
             <div className="ToStaging">
@@ -15,8 +24,14 @@ class ToStaging extends Component<IToStagingProps,any> {
         )
     }
 }
+
 const mapStateToProps = (state: IState) => ({
-    auth: state.managementState.auth
+    auth: state.managementState.auth,
+    cohortList: state.dashboardState.toStaging.cohortList
 });
+
+const mapDispatchToProps = {
+    getCohorts
+}
 
 export default connect(mapStateToProps)(ToStaging);
