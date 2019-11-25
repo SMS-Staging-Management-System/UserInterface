8f79e07204e6a70d8ad241adeca840f724c237a5
AdamDKing <adamdentonking@gmail.com>
2019-11-25 14:39:01 -0500

    apply prettier format to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/** @prettier */
 import * as React from 'react';
 import './include/bootstrap';
 import 'react-toastify/dist/ReactToastify.css';
@@ -5,9 +6,7 @@ import './App.scss';
 import { store } from './Store';
 import AppNav from './components/nav/nav.component';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
-
 import { ToastContainer, toast } from 'react-toastify';
 import { ManagementRoutes } from './routes/Management.routes';
 import { SurveyRoutes } from './routes/Survey.routes';
@@ -17,23 +16,24 @@ import { DashboardRoutes } from './routes/Dashboard.routes';
 class App extends React.Component {
   public render() {
     return (
-      <Provider store={store as any} >
+      <Provider store={store as any}>
         <BrowserRouter>
           <div>
             <AppNav />
 
             <div id="main-content-container">
               <Switch>
-
-                <Route path='/management' component={ManagementRoutes} />
-                <Route path='/interview' component={InterviewRoutes} />
-                <Route path='/surveys' component={SurveyRoutes} />
-                <Route path='/dashboard' component={DashboardRoutes} />
-                <Route path='/' component={DashboardRoutes} />
-
+                <Route path="/management" component={ManagementRoutes} />
+                <Route path="/interview" component={InterviewRoutes} />
+                <Route path="/surveys" component={SurveyRoutes} />
+                <Route path="/dashboard" component={DashboardRoutes} />
+                <Route path="/" component={DashboardRoutes} />
               </Switch>
             </div>
-            <ToastContainer autoClose={2000} position={toast.POSITION.BOTTOM_RIGHT} />
+            <ToastContainer
+              autoClose={2000}
+              position={toast.POSITION.BOTTOM_RIGHT}
+            />
           </div>
         </BrowserRouter>
       </Provider>
