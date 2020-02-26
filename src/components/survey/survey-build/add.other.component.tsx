b484657eeb64651c4976d613c8f919b6d29378f9
Loop421 <lopez.e421@gmail.com>
2020-02-26 09:01:32 -0500

    Updating survey builder

--- src/components/survey/survey-build/add.other.component.tsx
+++ src/components/survey/survey-build/add.other.component.tsx
@@ -17,10 +17,13 @@ export class AddOther extends React.Component<AddProps, any> {
   // pass in function call for destroying the component and rendering a new one in its place. 
   selectType = (event) => {
     // if(this.props.selfDestruct)this.props.selfDestruct(this.props.index, event.target.value);
+    let value = event.target.value;
     if (this.props.name === 'Select Question Type') {
-      this.props.parentFunction(event.target.value)
+      
+      
+      this.props.parentFunction(value)
     } else {
-      this.props.parentFunction(event.target.value, this.props.index);
+      this.props.parentFunction(value, this.props.index);
     }
   }
 
@@ -29,13 +32,47 @@ export class AddOther extends React.Component<AddProps, any> {
       <div>
         <button type="button" className="btn rev-btn dropdown-toggle typeSelect" data-toggle="dropdown" >{this.props.name}</button>
         <div className="dropdown-menu" aria-labelledby="examples-dropdown">
-          <Button type="button" onClick={this.selectType} value="True/False" className=" dropdown-item nav-dropdown">True/False</Button>
-          <Button type="button" onClick={this.selectType} value="Multiple Choice" className=" dropdown-item nav-dropdown">Multiple Choice</Button>
-          <Button type="button" onClick={this.selectType} value="Checkbox Multiple Answer" className=" dropdown-item nav-dropdown">Checkbox Multiple Answers</Button>
-          <Button type="button" onClick={this.selectType} value="Rating" className=" dropdown-item nav-dropdown">Rating</Button>
-          <Button type="button" onClick={this.selectType} value="Feedback" className=" dropdown-item nav-dropdown">Feedback</Button>
-          <Button type="button" onClick={this.selectType} value="Yes/No" className=" dropdown-item nav-dropdown">Yes/No</Button>
-          <Button type="button" onClick={this.selectType} value="Strongly Agree/Disagree" className=" dropdown-item nav-dropdown">Strongly Agree/Disagree</Button>
+          <Button type="button" 
+            onClick={this.selectType} 
+            value="True/False" 
+            className=" dropdown-item nav-dropdown"
+          >True/False</Button>
+          <Button 
+            type="button" 
+            onClick={this.selectType} 
+            value="Multiple Choice" 
+            className=" dropdown-item nav-dropdown"
+          >Multiple Choice</Button>
+          <Button 
+            type="button" 
+            onClick={this.selectType} 
+            value="Checkbox Multiple Answer" 
+            className=" dropdown-item nav-dropdown"
+          >Checkbox Multiple Answers</Button>
+          <Button 
+            type="button" 
+            onClick={this.selectType} 
+            value="Rating" 
+            className=" dropdown-item nav-dropdown"
+          >Rating</Button>
+          <Button 
+            type="button" 
+            onClick={this.selectType} 
+            value="Feedback" 
+            className=" dropdown-item nav-dropdown"
+          >Feedback</Button>
+          <Button 
+            type="button" 
+            onClick={this.selectType} 
+            value="Yes/No" 
+            className=" dropdown-item nav-dropdown"
+          >Yes/No</Button>
+          <Button 
+            type="button" 
+            onClick={this.selectType} 
+            value="Strongly Agree/Disagree" 
+            className=" dropdown-item nav-dropdown"
+          >Strongly Agree/Disagree</Button>
         </div>
       </div>
 
